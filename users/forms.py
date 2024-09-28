18576e385ca40a3be067adaf8c263fef83901836
test1Android studio <nazariamirhossein99@gmail.com>
2024-09-28 16:46:39 +0330

    Rapoll1-django

---
+++ users/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import User
+
+class SignUpForm(forms.ModelForm):
+    password_confirm = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+        widgets = {
+            'password': forms.PasswordInput(),
+        }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password_confirm = cleaned_data.get('password_confirm')
+
+        if password != password_confirm:
+            raise forms.ValidationError('Passwords do not match!')
