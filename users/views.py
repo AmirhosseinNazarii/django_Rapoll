85f44fbed0de45711b667af218f47f7dc8fec4d1
test1Android studio <nazariamirhossein99@gmail.com>
2024-09-28 21:38:17 +0330

    rapoll2jangoHash

--- users/views.py
+++ users/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render, redirect
 from .forms import SignUpForm
+from django.contrib.auth.hashers import make_password
 
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            form.save()
+            # هش کردن رمز عبور قبل از ذخیره
+            user = form.save(commit=False)
+            user.password = make_password(form.cleaned_data['password'])
+            user.save()
             return redirect('signup_success')
     else:
         form = SignUpForm()
 
     return render(request, 'users/signup.html', {'form': form})
+
 def signup_success(request):
     return render(request, 'users/signup_success.html')
+
