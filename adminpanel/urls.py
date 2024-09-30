b0933ca42631455d36684ee0b9d046515fc4aa83
Amirhossein <nazariamirhossein99@gmail.com>
2024-09-30 15:08:50 +0330

    panel admin delete and active user ezafe shod

--- adminpanel/urls.py
+++ adminpanel/urls.py
@@ -1,6 +1,10 @@
+# adminpanel/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('panel1/', views.panel1, name='panel1'),
+    path('edit_user/<int:user_id>/', views.edit_user, name='edit_user'),
+    path('delete_user/<int:user_id>/', views.delete_user, name='delete_user'),
+    path('toggle_status/<int:user_id>/', views.toggle_status, name='toggle_status'),
 ]
--- adminpanel/views.py
+++ adminpanel/views.py
@@ -1,4 +1,24 @@
-from django.shortcuts import render
+# adminpanel/views.py
+from django.shortcuts import render, get_object_or_404, redirect
+from users.models import User, UserDetails
+
 
 def panel1(request):
-    return render(request, 'adminpanel/panel1.html')
+    users = User.objects.all()
+    return render(request, 'adminpanel/panel1.html', {'users': users})
+
+def edit_user(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    # کد ویرایش کاربر
+    return redirect('panel1')
+
+def delete_user(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    user.delete()
+    return redirect('panel1')
+
+def toggle_status(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    user.status = not user.status  # تغییر وضعیت کاربر
+    user.save()
+    return redirect('panel1')
