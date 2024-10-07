999e8253b0b841746be08994f4362bbbe5327eb0
Amirhossein <nazariamirhossein99@gmail.com>
2024-10-07 10:25:51 +0330

    table transaction ezafe shod. shokre khoda

--- users/models.py
+++ users/models.py
@@ -39,3 +39,22 @@ class BlockActive(models.Model):
 
     def __str__(self):
         return f'Block {self.block_number} in {self.neighborhood}, {self.city}'
+    
+    # مدل جدید برای ذخیره اطلاعات خرید و فروش بلوک‌ها
+class Transaction(models.Model):
+    seller = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='transactions_selling')  # فروشنده
+    buyer = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='transactions_buying')  # خریدار
+    block = models.ForeignKey('users.BlockActive', on_delete=models.CASCADE, related_name='transactions')  # بلوک معامله شده
+    price = models.DecimalField(max_digits=10, decimal_places=2)  # قیمت بلوک
+    status = models.BooleanField(default=False)  # وضعیت معامله (False: باز، True: تکمیل‌شده)
+    
+    # اطلاعات بلوک برای دسترسی مستقیم به جزئیات
+    city = models.CharField(max_length=50)
+    neighborhood = models.CharField(max_length=100)
+    street = models.CharField(max_length=100)
+    alley = models.CharField(max_length=100)
+    
+    created_at = models.DateTimeField(auto_now_add=True)  # تاریخ ثبت معامله
+
+    def __str__(self):
+        return f'Transaction between {self.seller} and {self.buyer} for Block {self.block.block_number}'
